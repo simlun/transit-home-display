0dc3fdfb946e33330ec2b39fc1c635c922f3fb16
Simon Lundmark <simon.lundmark@gmail.com>
2015-06-30 21:08:09 +0200

    Cleanup: add comment blocks

--- NET/sketchbooks/netmicro/netmicro.ino
+++ NET/sketchbooks/netmicro/netmicro.ino
@@ -4,33 +4,56 @@
 #include <string.h>
 #include "utility/debug.h"
 
-// These are the interrupt and control pins
-#define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin!
+/**
+ * Configure Pins
+ **/
+
+// MUST be an interrupt pin!
+#define ADAFRUIT_CC3000_IRQ   3
+
 // These can be any two pins
 #define ADAFRUIT_CC3000_VBAT  5
 #define ADAFRUIT_CC3000_CS    10
+
 // Use hardware SPI for the remaining pins
 // On an UNO, SCK = 13, MISO = 12, and MOSI = 11
-Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
-                                         SPI_CLOCK_DIVIDER); // you can change this clock speed
 
-// Include WLAN_SSID and WLAN_PASS as #define's in this file:
+
+/**
+ * Global CC3000 Instance
+ **/
+
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ,
+                                         ADAFRUIT_CC3000_VBAT, SPI_CLOCK_DIVIDER);
+
+
+/**
+ * Configure WiFi Credentials
+ *
+ * Set WLAN_SSID, WLAN_PASS and WLAN_SECURITY as #define's in this #included file.
+ */
+
 #include "wlan_credentials.h"
 
-// Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
-#define WLAN_SECURITY   WLAN_SEC_WPA2
+
+/**
+ * General Configuration
+ */
 
 #define IDLE_TIMEOUT_MS  3000
-
 #define WEBSITE      "www.adafruit.com"
 #define WEBPAGE      "/testwifi/index.html"
 
 
+/**
+ * Declarations
+ */
 uint32_t ip;
+
 
 void setup(void) {
   Serial.begin(115200);
-  Serial.println(F("Hello, CC3000!\n")); 
+  Serial.println(F("Hello, CC3000!\n"));
 
   Serial.println(F("\nInitializing..."));
   if (!cc3000.begin()) {
