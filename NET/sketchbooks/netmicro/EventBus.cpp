7b8f87f028fcef8e90a50cf26d3d8bf1a69bcce9
Simon Lundmark <simon.lundmark@gmail.com>
2015-09-29 04:26:12 +0200

    Add debug logging to event bus

--- NET/sketchbooks/netmicro/EventBus.cpp
+++ NET/sketchbooks/netmicro/EventBus.cpp
@@ -3,6 +3,7 @@
 EventHandler::EventHandler(EventBus * eventBus) : eventBus(eventBus) {}
 
 void EventBus::registerHandler(EventHandler * handler) {
+    Serial.println(F("Registering event bus event handler"));
     eventHandlers[handler->event()] = handler;
 }
 
@@ -12,9 +13,11 @@ void EventBus::post(Event event) {
 
 void EventBus::process() {
     if (!eventQueue.empty()) {
+        Serial.println(F("Found an event on the queue"));
         Event event = eventQueue.front();
         eventQueue.pop();
         if (eventHandlers.count(event) == 1) {
+            Serial.println(F("Calling event handler"));
             EventHandler * handler = eventHandlers[event];
             handler->handle();
         }
