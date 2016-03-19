8e538bc8981138028c554003bb522397e88d7880
Simon Lundmark <simon.lundmark@gmail.com>
2016-03-20 00:24:04 +0100

    NET: Disable debugging

--- NET/sketchbooks/netmicro/debug.h
+++ NET/sketchbooks/netmicro/debug.h
@@ -2,9 +2,9 @@
 #define DEBUG_H
 
 // Change boolean to enable or disable debug code
-#define DEBUG if (true)
+#define DEBUG if (false)
 
 // Set to 1 to enable, 0 to disable
-#define DEBUG_BLOCK 1
+#define DEBUG_BLOCK 0
 
 #endif
