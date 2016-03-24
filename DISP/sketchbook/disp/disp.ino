eaeb2367fb856efcbcea4be3657ae27fd61d90de
Simon Lundmark <simon.lundmark@gmail.com>
2016-03-24 12:42:43 +0100

    DISP: Set up the DISP as an I2C slave

--- DISP/sketchbook/disp/disp.ino
+++ DISP/sketchbook/disp/disp.ino
@@ -1,10 +1,11 @@
 #include <Wire.h> 
 #include <LiquidCrystal_SI2C.h>
 
-#define MOTION_DETECTION_PIN 2
-#define MOTION_DETECTION_GRACE_PERIOD_MS 30000
-volatile boolean motionDetected = false;
-unsigned long msSinceLastMotionDetected = 0;
+/**********************************************************************
+ * Data
+ **********************************************************************/
+
+// LCD:
 
 boolean backlightOn = false;
 
@@ -13,11 +14,28 @@ boolean backlightOn = false;
 // SDA: Arduino pin 9
 LiquidCrystal_SI2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
-void setup() {
+
+// Motion Detection:
+
+#define MOTION_DETECTION_PIN 2
+#define MOTION_DETECTION_GRACE_PERIOD_MS 30000
+volatile boolean motionDetected = false;
+unsigned long msSinceLastMotionDetected = 0;
+
+
+
+
+/**********************************************************************
+ * Functions
+ **********************************************************************/
+
+/**
+ * LCD
+ **/
+
+void setupLCD() {
   lcd.begin(20, 4);
   
-  lcd.noBacklight();
-
   lcd.home();
   lcd.print("12345678901234567890");  
   lcd.setCursor(0, 1);
@@ -26,30 +44,84 @@ void setup() {
   lcd.print("12345678901234567890");
   lcd.setCursor(0, 3);
   lcd.print("12345678901234567890");
-  
-  attachInterrupt(digitalPinToInterrupt(MOTION_DETECTION_PIN), interruptServiceRoutine, RISING);
+
+  delay(2000);
+  lcd.clear();
+  delay(500);
+  lcd.noBacklight();
 }
 
-void interruptServiceRoutine() {
-  motionDetected = true;
-}
-
-void loop() {
-  if (motionDetected) {
-    motionDetected = false;
-    msSinceLastMotionDetected = millis();
-    lcd.backlight();
-    backlightOn = true;
-  }
-  
+void maybeDisableLCDBacklight() {
   if (backlightOn && millis() - msSinceLastMotionDetected > MOTION_DETECTION_GRACE_PERIOD_MS) {
     lcd.noBacklight();
     backlightOn = false;
   }
 }
 
+
+/**
+ * Motion Detection
+ **/
+
+void setupMotionDetection() {
+  attachInterrupt(digitalPinToInterrupt(MOTION_DETECTION_PIN), interruptServiceRoutine, RISING);  
+}
+
+void interruptServiceRoutine() {
+  motionDetected = true;
+}
+
+void motionDetection() {
+  if (motionDetected) {
+    motionDetected = false;
+    msSinceLastMotionDetected = millis();
+    lcd.backlight();
+    backlightOn = true;
+  }
+}
+
+
+/**
+ * I2C
+ **/
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  while (1 < Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  }
+  int x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
+}
+
+void setupI2CSlave() {
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);
+}
+
+
+/**
+ * Arduino
+ **/
+
+void setup() {
+  setupLCD();
+  setupMotionDetection();
+  setupI2CSlave();
+}
+
+void loop() {
+  motionDetection();
+  maybeDisableLCDBacklight();
+}
+
+
+
 // TODO
 // [x] Hello world
 // [x] Enable backlight with motion detector
-// [ ] Make this a hardware I2C slave
+// [x] Make this a hardware I2C slave
 // [ ] Design I2C protocol
