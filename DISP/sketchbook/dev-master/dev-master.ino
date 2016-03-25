ce2862111f82598063e257be6f3cea0ee4527dd7
Simon Lundmark <simon.lundmark@gmail.com>
2016-03-25 21:46:13 +0100

    DISP: Implement I2C protocol

--- DISP/sketchbook/dev-master/dev-master.ino
+++ DISP/sketchbook/dev-master/dev-master.ino
@@ -1,19 +1,41 @@
 #include <Wire.h>
 
-// Master writer
+#define DISP_I2C_SLAVE_ADDRESS 0x17
+
+byte I2C_PING = 0x01;
+byte I2C_CLEAR = 0x02;
+byte I2C_DATA = 0x03;
+byte I2C_REFRESH = 0x04;
+
+void transmitCmd(byte cmd) {
+  Wire.beginTransmission(DISP_I2C_SLAVE_ADDRESS);
+  Wire.write(cmd);
+  Wire.endTransmission();  
+}
+
+void transmitData(byte data) {
+  Wire.beginTransmission(DISP_I2C_SLAVE_ADDRESS);
+  Wire.write(I2C_DATA);
+  Wire.write(data);
+  Wire.endTransmission();  
+}
+
+void transmitData(char * data) {
+  int i = 0;
+  char c = data[i];
+  while (c != '\0') {
+    transmitData(c);
+    i++;
+    c = data[i];
+  }
+}
 
 void setup() {
-  Wire.begin(); // join i2c bus (address optional for master)
+  Wire.begin();
+
+  transmitCmd(I2C_CLEAR);
+  transmitData("Foo\nbar\nbaz hej svej lol roflmao!");
+  transmitCmd(I2C_REFRESH);
 }
 
-byte x = 0;
-
-void loop() {
-  Wire.beginTransmission(8); // transmit to device #8
-  Wire.write("x is ");        // sends five bytes
-  Wire.write(x);              // sends one byte
-  Wire.endTransmission();    // stop transmitting
-
-  x++;
-  delay(500);
-}
+void loop() {}
--- DISP/sketchbook/disp/disp.ino
+++ DISP/sketchbook/disp/disp.ino
@@ -1,18 +1,22 @@
 #include <Wire.h> 
 #include <LiquidCrystal_SI2C.h>
 
+#define DISP_I2C_SLAVE_ADDRESS 0x17
+
 /**********************************************************************
  * Data
  **********************************************************************/
 
 // LCD:
 
-boolean backlightOn = false;
+boolean backlightOn = true;
 
 // Software I2C for the LCD
 // SCL: Arduino pin 8
 // SDA: Arduino pin 9
 LiquidCrystal_SI2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+char lcdBuffer[4][20 + 1];
 
 
 // Motion Detection:
@@ -33,22 +37,52 @@ unsigned long msSinceLastMotionDetected = 0;
  * LCD
  **/
 
-void setupLCD() {
-  lcd.begin(20, 4);
-  
+void lcdRefresh() {
   lcd.home();
-  lcd.print("12345678901234567890");  
-  lcd.setCursor(0, 1);
-  lcd.print("12345678901234567890");
-  lcd.setCursor(0, 2);
-  lcd.print("12345678901234567890");
-  lcd.setCursor(0, 3);
-  lcd.print("12345678901234567890");
+  for (byte row = 0; row <= 3; row++) {
+    for (byte column = 0; column <= 19; column++) {
+      lcd.setCursor(column, row);
+      lcd.print(lcdBuffer[row][column]);
+      
+      if (column < 19) {
+        lcd.setCursor(column + 1, row);
+        lcd.print('.');
+      }
+      if (column < 18) {
+        lcd.setCursor(column + 2, row);
+        lcd.print('.');
+      }
+      if (column < 17) {
+        lcd.setCursor(column + 3, row);
+        lcd.print('.');
+      }
+      if (row < 3) {
+        lcd.setCursor(column, row + 1);
+        lcd.print('.');
+      }
+    }
+  }
+}
 
-  delay(2000);
-  lcd.clear();
-  delay(500);
-  lcd.noBacklight();
+void setupLCD() {
+  // Init
+  lcd.begin(20, 4);
+
+  // Fill buffer with whitespace
+  for (byte row = 0; row <= 3; row++) {
+    for (byte column = 0; column <= 19; column++) {
+      lcdBuffer[row][column] = ' ';
+    }
+  }
+  
+  // Make all rows proper null-terminated strings
+  lcdBuffer[0][20] = '\0';
+  lcdBuffer[1][20] = '\0';
+  lcdBuffer[2][20] = '\0';
+  lcdBuffer[3][20] = '\0';
+  
+  // Write buffer to LCD
+  lcdRefresh();
 }
 
 void maybeDisableLCDBacklight() {
@@ -85,21 +119,85 @@ void motionDetection() {
  * I2C
  **/
 
-// function that executes whenever data is received from master
-// this function is registered as an event, see setup()
-void receiveEvent(int howMany) {
-  while (1 < Wire.available()) { // loop through all but the last
-    char c = Wire.read(); // receive byte as a character
-    Serial.print(c);         // print the character
+byte I2C_PING = 0x01;
+byte I2C_CLEAR = 0x02;
+byte I2C_DATA = 0x03;
+byte I2C_REFRESH = 0x04;
+
+byte bufferRow = 0;
+byte bufferCol = 0;
+
+void pong() {
+  lcd.clear();
+  lcd.home();
+  lcd.print("Pong!");
+  delay(2000);
+}
+
+void lcdClear() {
+  lcd.clear();
+  lcd.home();
+  bufferRow = 0;
+  bufferCol = 0;
+  
+  // Fill buffer with whitespace
+  for (byte row = 0; row <= 3; row++) {
+    for (byte column = 0; column <= 19; column++) {
+      lcdBuffer[row][column] = ' ';
+    }
   }
-  int x = Wire.read();    // receive byte as an integer
-  Serial.println(x);         // print the integer
+}
+
+void error(char * msg) {
+  lcd.clear();
+  lcd.home();
+  lcd.print("ERROR:");
+  lcd.setCursor(0, 1);
+  lcd.print(msg);
+  while (true);
+}
+
+void receiveEvent(int byteCount) {
+  byte b1 = Wire.read();
+  if (byteCount == 1) {
+    if (b1 == I2C_PING) {
+      pong();
+    } else if (b1 == I2C_CLEAR) {
+      lcdClear();
+    } else if (b1 == I2C_REFRESH) {
+      lcdRefresh();
+    } else {
+      error("Unexpected byte");
+    }
+  } else if (byteCount == 2) {
+    if (b1 != I2C_DATA) {
+      error("Expected I2C_DATA");
+    } else {
+      char c = Wire.read();
+      
+      if (bufferCol == 20 || c == '\n') {
+        bufferCol = 0;
+        bufferRow++;
+      }
+      if (bufferRow == 4) {
+        // There's nothing past the 4th row
+        return;
+      }
+      if (c == '\n') {
+        return;
+      }
+      
+      lcdBuffer[bufferRow][bufferCol] = c;
+      bufferCol++;
+    }
+  } else {
+    error("Unexpected byteCount");
+  }
 }
 
 void setupI2CSlave() {
-  Wire.begin(8);                // join i2c bus with address #8
-  Wire.onReceive(receiveEvent); // register event
-  Serial.begin(9600);
+  Wire.begin(DISP_I2C_SLAVE_ADDRESS);
+  Wire.onReceive(receiveEvent);
 }
 
 
@@ -117,11 +215,3 @@ void loop() {
   motionDetection();
   maybeDisableLCDBacklight();
 }
-
-
-
-// TODO
-// [x] Hello world
-// [x] Enable backlight with motion detector
-// [x] Make this a hardware I2C slave
-// [ ] Design I2C protocol
